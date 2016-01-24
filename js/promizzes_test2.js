ddd6a115154956a8c921cb90d6101d9cf1b419a6
Muzietto <marco_faustinelli@yahoo.com>
2016-01-24 20:53:23 +0100

    ajax tests not satisfactory

--- js/promizzes_test2.js
+++ js/promizzes_test2.js
@@ -1,13 +1,62 @@
 (function() {
   'use strict';
 
-  define(['promizzes2', 'chai'], function(promizzes, chai) {
+  define(['promizzes2', 'chai', 'utils'], function(promizzes, chai, utils) {
     var expect  = chai.expect;
     var promise = promizzes.promise;
     var fulfill = promizzes.fulfill;
     var depend  = promizzes.depend;
+    var ajax = utils.ajax;
+    var request = utils.request;
 
     describe('a promises system with every logic inside static methods', function() {
+
+      it('does ajax straight away', function(done) {
+        var data = ajax('http://localhost:8080/json/user.json');
+        var expectation = depend(data, expected({"name":"Marco","age":53,"town":"milano"}));
+        depend(expectation, execute(done));
+      });
+
+      xit('does chained ajax', function(done) {
+        var data = ajax('http://localhost:8080/json/user.json');
+        var expectation1 = depend(data, function(_) {
+          return ajax('http://localhost:8080/json/' + data.value.town + '.json');
+        });
+        var expectation2 = depend(expectation1, expected({"name":"Milano","population":1500000}));
+      
+        
+        depend(expectation2, execute(done));
+      });
+
+      it('does ajax when resolved', function(done) {
+        var req = request();
+        var data = depend(req, expected('http://localhost:8080/json/user.json'));
+        var expectation2 = depend(data, expected('{"name":"Marco","age":53,"town":"milano"}'));
+        depend(expectation2, execute(done));
+        fulfill(req, 'http://localhost:8080/json/user.json');
+      });
+
+      function expected(expected) {
+        return function(actual) {
+          var result = promise();
+          expect(actual).to.be.eql(expected);
+          console.log('promizzes2 test: ' + actual);
+          fulfill(result, actual);
+          //depend(result, function(_) {
+            
+          //});
+          return result;
+        };
+      }
+      
+      function execute(cb) {
+        return function executor(_) {
+          var result = promise();
+          fulfill(result, cb());
+          return result;
+        };
+      }
+
       it('builds a simple chain', function(done) {
 
         var sidePromise = promise();
@@ -31,11 +80,6 @@
         // depend(promise, fapb) // fapb :: a -> promise b
         var areaPromise = depend(sidePromise, makeArea);
         var printPromise = depend(areaPromise, printStuff);
-
-        // side
-        //  .then(makeArea)
-        //  .then(printStuff)
-        //  .then(beDone);
 
         fulfill(sidePromise, 5);
         var donePromise = depend(printPromise, beDone);
